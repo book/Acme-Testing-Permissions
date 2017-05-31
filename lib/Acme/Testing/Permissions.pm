c5015b3c0b9c4062e8b83828b7a81f0cf7dac05b
Philippe Bruhat (BooK) <book@cpan.org>
2017-05-31 09:41:53 +0200

    Initial revision

---
+++ lib/Acme/Testing/Permissions.pm
@@ -0,0 +1,15 @@
+package Acme::Testing::Permissions;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Acme::Testing::Permissions - Test target for PAUSE permissions
+
+=head1 DESCRIPTION
+
+Testing testing 1 2 3
+
+=cut
---
+++ lib/Acme/Testing/Permissions/SubModule.pm
@@ -0,0 +1,15 @@
+package Acme::Testing::Permissions::SubModule;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Acme::Testing::Permissions::SubModule - Another test target for PAUSE permissions
+
+=head1 DESCRIPTION
+
+Testing testing 1 2 3
+
+=cut
